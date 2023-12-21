9fe7ed45a50c84e90027d1af42d65c1c98140480
Arne Bogaerts <r0946572@student.thomasmore.be>
2023-12-21 17:50:01 +0100

    Aanpassingen usertest

--- myproject/test.py
+++ myproject/test.py
@@ -2,21 +2,31 @@ import requests
 import json
 
 BASE_URL = "https://api-arnebogaerts.cloud.okteto.net"
+# BASE_URL = "http://127.0.0.1:8000"
+
 
 def create_test_user():
     username = "testuser"
     password = "testpassword"
-    user_data = {"username": username, "password": password}
-    response = requests.post(f"{BASE_URL}/users/", json=user_data)
-    assert response.status_code == 200
-    return username, password
+    response = requests.post(f"{BASE_URL}/users/", json={"username": username, "password": password})
+    assert response.status_code == 200, f"Fout bij het aanmaken van testgebruiker: {response.text}"
+    user_id = response.json()["id"]
+    return username, password, user_id
 
-def get_access_token():
-    username, password = create_test_user()  # Creëert een testgebruiker
+username, password, user_id = create_test_user()
+
+def get_access_token(username, password):
     response = requests.post(f"{BASE_URL}/token", data={"username": username, "password": password})
-    assert response.status_code == 200
-    return response.json().get("access_token")
+    assert response.status_code == 200, f"Fout bij het ophalen van token: {response.text}"
+    return response.json()["access_token"]
 
+token = get_access_token(username, password)
+
+def test_read_users():
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(f"{BASE_URL}/users/", headers=headers)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 def test_read_cds():
     response = requests.get(f"{BASE_URL}/cds/")
@@ -34,10 +44,8 @@ def test_read_reviews_by_cd():
     assert response.status_code == 200
     assert isinstance(response.json(), list)  # Hier verwacht ik een lijst van reviews voor een specifieke CD
 
+def delete_test_user(user_id):
+    response = requests.delete(f"{BASE_URL}/users/{user_id}")
+    assert response.status_code in [200, 204], f"Fout bij het verwijderen van testgebruiker: {response.status_code}, {response.text}"
 
-def test_read_users():
-    token = get_access_token()
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{BASE_URL}/users/", headers=headers)
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+delete_test_user(user_id)
